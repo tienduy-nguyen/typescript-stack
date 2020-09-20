85ac6d9bab393a681fe1174946e330e6a7fc6cd0
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-09-20 22:51:51 +0200

    Update

---
+++ array/array.ts
@@ -0,0 +1,5 @@
+const carMakers: string[] = [];
+const carMakers2 = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+const carsByMake2: string[][] = [];
