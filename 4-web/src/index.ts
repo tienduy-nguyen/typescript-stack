6ee8feddbbd86ef7a9f49285e357fdaa087b3cd4
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-05 17:37:31 +0100

    Add events for User class

---
+++ 4-web/src/index.ts
@@ -0,0 +1,7 @@
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+user.on('change', () => {});
+user.on('change', () => {});
+
+console.log(user);
--- 4-web/src/models/User.ts
+++ 4-web/src/models/User.ts
@@ -5,13 +5,20 @@ export interface UserProps {
 }
 
 const rootUrl = 'http://localhost:3000/users';
-
+type Callback = () => void; // A function take no argument and return a void
 export class User {
+  events: {[key: string]: Callback[]} = {}; // assign key index
   constructor(private data: UserProps) {};
   get(propName: string): number | string {
     return '';
   }
   set(update: UserProps): void{
     Object.assign(this.data, update)
+  }
+  on(eventName: string, callback: Callback): void{
+    const handlers = this.events[eventName] || []; // Calback[] or undefined
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+
   }
 }
