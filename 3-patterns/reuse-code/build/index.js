8b063d9528789ff6f1b48d60ae9b5c813f517289
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-05 10:50:48 +0100

    Readfile csv

--- 3-patterns/reuse-code/build/index.js
+++ 3-patterns/reuse-code/build/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('Hi there!');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const csvFile = 'football.csv';
+const data = fs_1.default.readFileSync(csvFile, { encoding: 'utf-8' })
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+console.log(data);
+console.log(data[0]);
--- 3-patterns/reuse-code/src/index.ts
+++ 3-patterns/reuse-code/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hi there!')
+import fs from 'fs';
+
+const csvFile = 'football.csv'
+const data = fs.readFileSync(csvFile,{encoding: 'utf-8'})
+.split('\n')
+.map((row: string): string[] =>{
+  return row.split(',')
+})
+console.log(data)
+console.log(data[0])
