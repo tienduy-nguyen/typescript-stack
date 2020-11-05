ed8a529add7843048a807557a8d5914133bcc794
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-05 12:13:23 +0100

    Minor fix

---
+++ 3-patterns/reuse-code/src/CsvFileReader.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+export class CsvFileReader {
+  data: string[][] = [];
+  fileName: string;
+  constructor(fileName: string) {
+    this.fileName = fileName;
+  }
+  read(): void {
+    this.data = fs
+      .readFileSync(this.fileName, { encoding: 'utf-8' })
+      .split('\n')
+      .map((row: string): string[] => {
+        return row.split(',');
+      });
+  }
+}
