52147333a6080b6070d080153ba40ee86a9c8c86
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-06 10:45:36 +0100

    Use generics for Sync

--- 4-web/src/models/Sync.ts
+++ 4-web/src/models/Sync.ts
@@ -1,12 +1,16 @@
 import axios, { AxiosError, AxiosPromise } from 'axios';
 import { UserProps } from './User';
-export class Sync {
+
+interface HasId {
+  id: number | string;
+}
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
   fetch(id: string | number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
     axios.get(`${this.rootUrl}/${id}`).catch((err) => {
       if (err.response.status === 404) {
