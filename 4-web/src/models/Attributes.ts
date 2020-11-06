5312fcfa44248e4c3b3553f3d5a69c6207135c0f
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-06 11:56:06 +0100

    Using generics for keyof type

--- 4-web/src/models/Attributes.ts
+++ 4-web/src/models/Attributes.ts
@@ -2,8 +2,8 @@ import { IndexSignature } from './User';
 
 export class Attributes<T extends IndexSignature> {
   constructor(private data: T) {}
-  get(propName: string): number | string | undefined {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
   set(update: T): void {
     Object.assign(this.data, update);
