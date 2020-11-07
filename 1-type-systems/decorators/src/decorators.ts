6384ad770abba6fcc6a3d1be149d21c96cfc842d
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-07 18:12:16 +0100

    Init decorator in TWé

---
+++ 1-type-systems/decorators/src/decorators.ts
@@ -0,0 +1,18 @@
+class Boat {
+  color: string = 'red';
+  get formattedColor(): string {
+    return `This boats  color is ${this.color}`;
+  }
+  @testDecorator
+  pilot(): void {
+    console.log('swish');
+  }
+}
+
+function testDecorator(target: any, key: string): void {
+  console.log('Target:', target);
+  console.log('Key:', key);
+}
+
+const boat = new Boat();
+boat.pilot();
