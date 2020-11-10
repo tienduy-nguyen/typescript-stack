95c088c247d781371248be019c500d59828680cc
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-10 15:50:30 +0100

    Using functional components

--- 6-react/rrts/src/index.tsx
+++ 6-react/rrts/src/index.tsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps{
   color?: string;
 }
-class App extends React.Component<AppProps>{
 
-  state = {counter: 0}
-
-  onIncrement = () =>{
-    this.setState({counter: this.state.counter +1 })
-  }
-  onDecrement = () =>{
-    this.setState({counter: this.state.counter -1})
-  }
-  render(){
-    return(
-      <div>
-        <button onClick = {this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    )
-  }
+const App = (props: AppProps): JSX.Element =>{
+  return (
+    <div>{props.color}</div>
+  )
 }
+// class App extends React.Component<AppProps>{
+//   state = {counter: 0}
+
+//   onIncrement = () =>{
+//     this.setState({counter: this.state.counter +1 })
+//   }
+//   onDecrement = () =>{
+//     this.setState({counter: this.state.counter -1})
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <button onClick = {this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     )
+//   }
+// }
 
 ReactDOM.render(
   <App color="red"></App>,
