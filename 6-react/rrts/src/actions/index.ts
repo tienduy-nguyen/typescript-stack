375b3f8912e72f9c0377aba08bed3f14a6f73526
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-10 16:16:33 +0100

    Action types enum

--- 6-react/rrts/src/actions/index.ts
+++ 6-react/rrts/src/actions/index.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
-export enum ActionTypes {
-  FETCH = 'fetch',
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
 }
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const res = await axios.get(url);
+    const res = await axios.get<Todo[]>(url);
     dispatch({
-      type: ActionTypes.FETCH,
+      type: ActionTypes.FETCH_TODOS,
       payload: res.data,
     });
   };
