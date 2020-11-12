a8a5e73b4b624a7e6410586abba96b4fd7174436
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-12 17:06:34 +0100

    Update controller

---
+++ 5-nodejs/error-handler/src/controllers/ThemeA.ts
@@ -0,0 +1,8 @@
+import { ErrorHandler } from '../models/ErrorHandler';
+
+class ThemeAController {
+  defaultMethod() {
+    throw new ErrorHandler(501, 'Not implemented method');
+  }
+}
+export = new ThemeAController();
