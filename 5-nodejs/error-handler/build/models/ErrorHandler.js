dde5d7db7a07c854e936a489122f517a20ce0dc4
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2020-11-12 17:04:55 +0100

    Update ErrorHandler

---
+++ 5-nodejs/error-handler/build/models/ErrorHandler.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ErrorHandler = void 0;
+class ErrorHandler extends Error {
+    constructor(statusCode, message) {
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+exports.ErrorHandler = ErrorHandler;
